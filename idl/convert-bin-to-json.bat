e675124b1994cf99b7be6293562fc9ce0d2fbd93
rbcunha79 <rbcunha@yahoo.com>
2023-07-28 17:22:03 -0300

    new parameters for JSON file generation

--- idl/convert-bin-to-json.bat
+++ idl/convert-bin-to-json.bat
@@ -1,5 +1,9 @@
 @echo off
-call flatc -t Common.fbs BankTransactions.fbs -- bankTransaction.bin
+call flatc --json Common.fbs BankTransactions.fbs ^
+--strict-json ^
+--defaults-json ^
+--natural-utf8 ^
+-- bankTransaction.bin
 
 if not %errorlevel%==0 goto end1
 
