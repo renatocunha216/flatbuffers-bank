fd825bfd7ac3d39022594f6a3114c1ce060ec8ce
rbcunha79 <rbcunha@yahoo.com>
2023-07-28 16:33:46 -0300

    fixed artifact creation script

--- idl/create-artifacts.bat
+++ idl/create-artifacts.bat
@@ -1,7 +1,11 @@
 @echo off
 call flatc --java Common.fbs BankTransactions.fbs
 
-if not errorlevel 0 goto end
+if not %errorlevel% == 0 goto end
+
+call flatc --cpp Common.fbs BankTransactions.fbs
+
+if not %errorlevel% == 0 goto end
 
 echo --------------------------------
 echo Successfully created artifacts
