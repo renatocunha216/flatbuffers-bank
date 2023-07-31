1c339e2c4a587328794a3b8660cfad1da87d728e
rbcunha79 <rbcunha@yahoo.com>
2023-07-31 16:04:54 -0300

    -

--- idl/create-artifacts.bat
+++ idl/create-artifacts.bat
@@ -5,7 +5,7 @@ if not %errorlevel% == 0 goto end
 
 rem call flatc --cpp Common.fbs BankTransactions.fbs
 
-if not %errorlevel% == 0 goto end
+rem if not %errorlevel% == 0 goto end
 
 echo --------------------------------
 echo Successfully created artifacts
