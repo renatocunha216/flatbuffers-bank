ea3072e5d55739f952d5a8364e01beec41a8d8d9
rbcunha79 <rbcunha@yahoo.com>
2023-10-27 14:55:46 -0300

    artifacts for c++ project

---
+++ cpp/src/main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : RBC
+// Version     :
+// Copyright   : Your copyright notice
+// Description : 
+//============================================================================
+
+#include "flatbuffers/flatbuffers.h"
+#include "Common_generated.h"
+#include "BankTransactions_generated.h"
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::cout << "Hello Flatbuffers!";
+	return 0;
+}
