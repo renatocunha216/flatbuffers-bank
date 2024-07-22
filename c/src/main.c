1bcc6fe23d98b91b663f2e9007ded7d39beef013
Renato Cunha <renato.rcunha@tivit.com>
2024-07-22 16:33:00 -0300

    -

---
+++ c/src/main.c
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : RBC
+// Version     :
+// Copyright   : Your copyright notice
+// Description : 
+//============================================================================
+
+//#include "flatbuffers/flatbuffers.h"
+#include "flatbuffers_common_reader.h"
+#include "flatbuffers_common_builder.h"
+#include "BankTransactions_reader.h"
+#include "BankTransactions_builder.h"
+#include "BankTransactions_verifier.h"
+#include "Common_reader.h"
+#include "Common_builder.h"
+#include "Common_verifier.h"
+
+#include <stdio.h>
+
+
+int main() {
+    printf("Hello Flatbuffers!");
+    return 0;
+}
