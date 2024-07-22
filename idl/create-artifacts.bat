aba32985716455995b4ecd60b643ba1c4ea9c7f9
Renato Cunha <renato.rcunha@tivit.com>
2024-07-22 16:31:41 -0300

    script to create artifacts for c language

--- idl/create-artifacts.bat
+++ idl/create-artifacts.bat
@@ -7,6 +7,11 @@ call flatc --cpp Common.fbs BankTransactions.fbs
 
 if not %errorlevel% == 0 goto end
 
+rem create artifacts for c language
+call flatcc -a BankTransactions.fbs
+
+if not %errorlevel% == 0 goto end
+
 echo --------------------------------
 echo Successfully created artifacts
 echo --------------------------------
