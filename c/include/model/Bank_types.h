ef612ee77773fdf728530004e70a78b60af3de0b
Renato Cunha <rbcunha@yahoo.com>
2024-07-23 23:30:08 -0300

    -

---
+++ c/include/model/Bank_types.h
@@ -0,0 +1,18 @@
+/*
+ * Bank_types.h
+ *
+ *  Created on: 23 de jul. de 2024
+ *      Author: Renato Cunha
+ */
+
+#ifndef INCLUDE_MODEL_BANK_TYPES_H_
+#define INCLUDE_MODEL_BANK_TYPES_H_
+
+typedef struct {
+	uint32_t code;
+	char name[25];
+} Bank;
+
+
+
+#endif /* INCLUDE_MODEL_BANK_TYPES_H_ */
--- c/src/main.c
+++ c/src/main.c
@@ -15,11 +15,41 @@
 #include "Common_reader.h"
 #include "Common_builder.h"
 #include "Common_verifier.h"
+#include "model/Bank_types.h"
 
 #include <stdio.h>
+#include <stdint.h>
+
+#undef ns
+#define ns(x) FLATBUFFERS_WRAP_NAMESPACE(flatbuffers, x) // Specified in the schema.
+
+void loadBanks(Bank* bank) {
+	Bank b = {code: 9313, name: "ABN AMRO S.A."};
+}
 
 
 int main() {
+
+	//AllBanks banks;
+
     printf("Hello Flatbuffers!");
+
+    uint8_t *buf;
+    size_t size;
+    flatcc_builder_t builder;
+    flatcc_builder_t* B;
+
+    // Initialize the builder object.
+    B = &builder;
+    flatcc_builder_init(B);
+
+	ns(DateTimeFbs_create(B, 1, 2, 3, 4 , 5 , 6));
+
+	// TODO: .....
+
+
+    free(buf);
+	flatcc_builder_clear(B);
+
     return 0;
 }
